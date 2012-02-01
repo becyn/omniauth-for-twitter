de67250be0c69ec9fc43ca46ef3827a6dd3ace13
becyn <yucalcium.c6@gmail.com>
2012-02-01 15:08:27 -0800

    rails new omniauth_for_twitter.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthForTwitter::Application.config.secret_token = '0d49c285820731e48c847ccff244390768cce9ed4a8a6fb0641811c81effa7e9e03d324d06f64472fcd1bdab743cb81bbe10e23443db8cd71781692f467f2154'
