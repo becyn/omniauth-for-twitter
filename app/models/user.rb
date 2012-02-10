fd857ec631ffd7ce8e6536fad2ce6b9f30e1ceb8
becyn <yucalcium.c6@gmail.com>
2012-02-10 14:55:57 -0800

    edited user model related files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+def self.create_with_omniauth(auth)
+     user = User.new
+     user.provider = auth[:provider]
+     user.uid = auth[:uid]
+     user.twitter_id = auth[:info][:nickname]
+     user.icon_url = auth[:info][:image]
+     user.save
+     return user
+   end
 end
--- db/migrate/20120202084256_create_users.rb
+++ db/migrate/20120202084256_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :provider
       t.string :uid
-      t.string :twitterID
+      t.string :twitter_id
       t.string :icon_url
 
       t.timestamps
