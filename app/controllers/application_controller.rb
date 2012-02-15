24197a7c3c8ea31315eaa86cf5b76bd76ae33939
becyn <yucalcium.c6@gmail.com>
2012-02-14 20:45:06 -0800

    edited some files related to sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-   def authenticated?
-     return false if session[:user_id].nil?
-     return true
-   end
-   helper_method :authenticated?
+  def authenticated?
+    return false if session[:user_id].nil? or session[:user_created_at].nil? or current_user.nil?
+    return true
+  end
+  helper_method :authenticated?
 
-   def current_user
-   @current_user ||= User.find_by_id(session[:user_id])
- end
- helper_method :current_user
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
 
- def authentication_filter
+  def authentication_filter
     return if authenticated?
     redirect_to root_path
- end
+  end
 end
